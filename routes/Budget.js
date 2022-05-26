ef5cd839eb67ea6dcac18dc56ab7c4a96798c5c2
Kamran <kamranasghar30728@gmail.com>
2022-05-26 12:01:03 +0500

    change2

--- routes/Budget.js
+++ routes/Budget.js
@@ -20,9 +20,6 @@ const {
   deleteBudget
 } = require("../controllers/Budget");
 router.get("/budget/get",cors(), auth, getBudget);
-// router.get("/budget/earningDetail/:id",cors(), auth, earningDetail);
-// router.get("/budget/expenseDetail/:id",cors(), auth, expenseDetail);
-//router.get("/budget/budgetDetail/:id/:pid",cors(), auth, getBudgetDetail);
 router.delete("/budget/delete/:id", cors(),auth, deleteBudget);
 router.post("/budget/create", cors(),auth, postBudget);
 router.patch("/budget/update/:id",cors(), auth, updateBudget);
